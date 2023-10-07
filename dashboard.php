b74ae6b6b7467bafbf47974ed7c12b7506cc204b
Manel <ea3eiz@ea3eiz.com>
2023-10-07 12:33:17 +0200

    1

---
+++ dashboard.php
@@ -0,0 +1,4 @@
+<?php 
+ exec("sudo systemctl restart hbmon");
+ exec("sudo systemctl restart hblink");
+header("Location:http://51.254.97.10:7010");
---
+++ dashboard_sin_cambios.php
@@ -0,0 +1,3 @@
+<?php 
+header("Location:http://51.254.97.10:7010");
+?>
