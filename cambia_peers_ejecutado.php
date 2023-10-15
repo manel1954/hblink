347b2ac5b608384a71e2472f1e08f0f49c06a4ed
Manel <ea3eiz@ea3eiz.com>
2023-10-15 22:55:17 +0200

    1

---
+++ cambia_peers_ejecutado.php
@@ -0,0 +1,18 @@
+<?php 
+
+session_start();
+
+$peers=($_POST["peers"]);
+
+exec("sudo sed -i '336c var miTexto2 = $peers; ' /opt/HBmonitor_OSCURO/index_template.html");
+
+
+
+
+
+
+exec("sudo systemctl restart hbmon.service");
+
+
+header("Location:dashboard_sin_cambios.php");	
+?>
