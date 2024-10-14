dd8e65d185595e62d9dd4c816e40ae7f4317b34a
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-10-14 10:54:37 +0200

    1

--- cambia_claro_oscuro.php
+++
@@ -1,6 +0,0 @@
-<?php 
-exec("sudo sh cambia_claro_oscuro.sh");
-header("Location:dashboard_sin_cambios.php");	
-?>
-
-	
--- cambia_oscuro_claro.php
+++
@@ -1,6 +0,0 @@
-<?php 
-exec("sudo sh cambia_oscuro_claro.sh");
-header("Location:dashboard_sin_cambios.php");	
-?>
-
-	
